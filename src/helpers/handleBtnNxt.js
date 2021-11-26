0d1d25c9d7a2eeeb110d6c6a59beaa55d28c9e75
Daniel Mendonça Acorroni <daniel.acorroni@hotmail.com>
2021-11-26 18:30:25 -0300

    Reseta bordas do botão ao clicar em próximo

--- src/helpers/handleBtnNxt.js
+++ src/helpers/handleBtnNxt.js
@@ -5,6 +5,10 @@ export default function handleBtnNxt() {
   if (questionSelector === FOUR) {
     history.push('/feedback');
   }
+  const wrongs = document.querySelectorAll('.wrong');
+  const correct = document.querySelector('.correct');
+  correct.classList.remove('correct-answer');
+  wrongs.forEach((wrong) => wrong.classList.remove('wrong-answer'));
   this.setState({
     questionSelector: questionSelector + 1,
   });

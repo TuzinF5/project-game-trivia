feef9bd0ff5cf3a7d51f504fb13604ad220e4d8e
Daniel Mendonça Acorroni <daniel.acorroni@hotmail.com>
2021-11-26 18:31:05 -0300

    Refatora lógica do timer

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -2,41 +2,45 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 export default class ProgressBar extends Component {
-  componentDidMount() {
-    this.startTimer();
+  constructor() {
+    super();
+
+    this.state = {
+      totalTime: 30,
+      countDown: (interval) => {
+        setInterval(() => {
+          const { totalTime } = this.state;
+          if (totalTime === 0) {
+            clearInterval(this.state.countDown);
+            console.log('aloo');
+            return;
+          }
+
+          this.setState({ totalTime: totalTime - 1 });
+        }, interval);
+      },
+    };
   }
 
-  startTimer() {
+  componentDidMount() {
+    const { countDown } = this.state;
+    countDown(1000);
+  }
+
+  componentDidUpdate(_prevProps, prevState) {
+    const { totalTime } = this.state;
     const { disableAnswers } = this.props;
-    const TOTAL_TIME = 31;
-    const INTERVAL = 1000;
-    let timer = TOTAL_TIME;
-    const countDown = setInterval(() => {
-      if (timer === 0) {
-        clearInterval(countDown);
-        return;
-      }
-
-      if (timer === 1) {
-        disableAnswers();
-      }
-
-      timer -= 1;
-
-      if (document.getElementById('counter')) {
-        document.getElementById('counter').innerText = timer;
-      }
-      if (document.getElementById('progressBar')) {
-        document.getElementById('progressBar').value = timer;
-      }
-    }, INTERVAL);
+    if (prevState.totalTime !== totalTime && totalTime === 0) {
+      disableAnswers();
+    }
   }
 
   render() {
+    const { totalTime } = this.state;
     return (
       <div>
-        <span id="counter" />
-        <progress value="0" max="30" id="progressBar" />
+        <span id="counter">{totalTime}</span>
+        <progress value={ totalTime } max="30" id="progressBar" />
       </div>
     );
   }

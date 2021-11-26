09c4c26d06f9cb7f36c9f65a0b08d9e87aa9a7ac
Daniel Mendonça Acorroni <daniel.acorroni@hotmail.com>
2021-11-26 19:11:22 -0300

    Refatora progressBar

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -2,52 +2,17 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 export default class ProgressBar extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      totalTime: 30,
-      countDown: (interval) => {
-        setInterval(() => {
-          const { totalTime } = this.state;
-          if (totalTime === 0) {
-            const { countDown } = this.state;
-            clearInterval(countDown);
-            console.log('aloo');
-            return;
-          }
-
-          this.setState({ totalTime: totalTime - 1 });
-        }, interval);
-      },
-    };
-  }
-
-  componentDidMount() {
-    const { countDown } = this.state;
-    const INTERVAL = 1000;
-    countDown(INTERVAL);
-  }
-
-  componentDidUpdate(_prevProps, prevState) {
-    const { totalTime } = this.state;
-    const { disableAnswers } = this.props;
-    if (prevState.totalTime !== totalTime && totalTime === 0) {
-      disableAnswers();
-    }
-  }
-
   render() {
-    const { totalTime } = this.state;
+    const { timer } = this.props;
     return (
       <div>
-        <span id="counter">{totalTime}</span>
-        <progress value={ totalTime } max="30" id="progressBar" />
+        <span id="counter">{timer}</span>
+        <progress value={ timer } max="30" id="progressBar" />
       </div>
     );
   }
 }
 
 ProgressBar.propTypes = {
-  disableAnswers: PropTypes.func.isRequired,
+  timer: PropTypes.number.isRequired,
 };
